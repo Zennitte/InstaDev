9c266fd877b1ede7ed0896f97cf66ae9ec0e309a
Tamashi19 <29raul19@gmail.com>
2021-07-02 17:34:21 -0300

    back cadastro iniciado

--- Controllers/CadastrarController.cs
+++ Controllers/CadastrarController.cs
@@ -1,7 +1,35 @@
+using InstaDev.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
 namespace InstaDev.Controllers
 {
-    public class CadastrarController
+    [Route("Usuario")]
+    public class CadastrarController : Controller
     {
-        
+        Usuario usuarioModel = new Usuario();
+        public IActionResult Index(){
+            return View();
+        }
+
+        [Route("Cadastrar")] 
+
+        public IActionResult Cadastrar(IFormCollection form){
+
+            Usuario novoUsuario = new Usuario();
+            novoUsuario.Nome= form["nome"];
+            novoUsuario.UserName=form["nick"];
+            novoUsuario.AtribuirEmail(form["email"]);
+            novoUsuario.AtribuirSenha(form["senha"]);
+            novoUsuario.AtribuirId();
+
+            usuarioModel.Cadastrar(novoUsuario);
+
+            return LocalRedirect("~/Usuario");
+            
+            
+
+        }
+
     }
 }
