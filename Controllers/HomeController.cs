7e76682617245ca2ed31b9290ac10304bcafdacd
Vinicius <vini.evangelista@live.com>
2021-07-05 15:54:37 -0300

    alterações

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using InstaDev.Models;
+// using InstaDev.Models;
 
 namespace InstaDev.Controllers
 {
